7a0e6688ddfb002e65733849ccec454b951ebd29
Salvijus Mazeikis <salvijussocial@gmail.com>
2020-02-23 20:09:01 +0000

    wow

--- drp.py
+++ drp.py
@@ -1,8 +1,11 @@
-from flask import Flask
+import os
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-
-def index():
-    return '<h1>Deployd</h1>'
+@app.route('/list', methods=['GET'])
+def read():
+    try:
+        return("FUCK YOU")
+    except Exception as e:
+        return f"An Error Occured: {e}"
