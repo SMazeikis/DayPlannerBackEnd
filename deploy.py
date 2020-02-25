a362571a831609710c55f4e7e3cab527ac68b420
Salvijus Mazeikis <salvijussocial@gmail.com>
2020-02-25 18:26:53 +0000

    testing external files

--- deploy.py
+++ deploy.py
@@ -1,32 +1,8 @@
-import os
-import requests
-import json
+import os, requests, json, pyrebase, firebase_admin
 from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
 from yelp.client import Client
-import pyrebase
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-
-config = {
-  "apiKey" : "AIzaSyDQT78_79sRW4nPY2hj_2zu6TX_V3VoD8k",
-  "authDomain" : "dayplanner-740b3.firebaseapp.com",
-  "databaseURL" : "https://dayplanner-740b3.firebaseio.com",
-  "projectId" : "dayplanner-740b3",
-  "storageBucket" : "dayplanner-740b3.appspot.com",
-  "messagingSenderId" : "930720206669",
-  "appId" : "1:930720206669:web:2d15a4b9f7ec24b1b7f104",
-  "measurementId" : "G-QZFMJ335XN"
-}
-
-firebase = pyrebase.initialize_app(config)
-
-# cred = credentials.Certificate('./nothing.json')
-# default_app = firebase_admin.initialize_app(cred)
-# db = firestore.client()
-# snapshot = db.collection('users').document("NUB1BGl5H0gqDi2JPYRzflO6GEF3")
-# print(snapshot.get().to_dict())
+from user import test
 
 MY_API_KEY = "b7wSeZeFykMFbPx-n7VpQD9gwi8EZWYYjaPEoa3ExSZv5c4LlGpANEhKR-sUW51218X6M3RO6t90tQGvbBAcX7pP9A3KnEo8FRfYZ1t1efZ_SXvj7POdAmmyqptTXnYx"
 
@@ -37,14 +13,6 @@ client = Client(MY_API_KEY)
 endpoint = "https://api.yelp.com/v3/businesses/search"
 header = {'Authorization': 'bearer %s' % MY_API_KEY}
 
-
-@app.route('/list', methods=['GET'])
-def read():
-    try:
-        return("hello")
-    except Exception as e:
-        return f"An Error Occured: {e}"
-
 @app.route('/parse_data', methods=['GET', 'POST'])
 def parse():
     try:
@@ -54,7 +22,21 @@ def parse():
     except:
         return(":)")
 
+@app.route('/userPreferences', methods=['GET', 'POST'])
+def userPreferences():
+    try:
+        data = request.get_json()
+        resolved_data = data
+        return(make_response(json.dumps(resolved_data)))
+    except:
+        return(":)")
 
+@app.route('/test', methods=['GET', 'POST'])
+def tester():
+    try:
+        return(test())
+    except:
+        return("that did not work :)")
 
 def restaurant_info(name):
     restaurant_names = []
--- user.py
+++ user.py
@@ -3,40 +3,12 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-config = {
-  "apiKey" : "AIzaSyDQT78_79sRW4nPY2hj_2zu6TX_V3VoD8k",
-  "authDomain" : "dayplanner-740b3.firebaseapp.com",
-  "databaseURL" : "https://dayplanner-740b3.firebaseio.com",
-  "projectId" : "dayplanner-740b3",
-  "storageBucket" : "dayplanner-740b3.appspot.com",
-  "messagingSenderId" : "930720206669",
-  "appId" : "1:930720206669:web:2d15a4b9f7ec24b1b7f104",
-  "measurementId" : "G-QZFMJ335XN"
-}
-
-# Use a service account
-cred = credentials.Certificate('dayplanner-backend/nothing.json')
-firebase_admin.initialize_app(cred)
+cred = credentials.Certificate('./confidential.json')
+default_app = firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
-firebase = pyrebase.initialize_app(config)
+snapshot = db.collection('users').document("NUB1BGl5H0gqDi2JPYRzflO6GEF3")
 
-
-# Get a reference to the auth service
-auth = firebase.auth()
-
-# Log the user in
-#user = auth.sign_in_with_email_and_password(email, password)
-
-
-# data to save
-data = {
-    "name": "Mortimer 'Morty' Smith"
-}
-
-# Pass the user's idToken to the push method
-#results = db.child("users").push(data, user['idToken'])
-users_ref = db.collection(u'users')
-docs = users_ref.stream()
-print(docs)
+def test():
+  return "test success :D"
