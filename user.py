ff8ec52251f3aee89b989a6e4b50bfa545666383
Salvijus Mazeikis <salvijussocial@gmail.com>
2020-02-29 15:07:28 +0000

    fixed confidential link

--- user.py
+++ user.py
@@ -3,7 +3,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-cred = credentials.Certificate('dayplanner-backend\confidential.json')
+cred = credentials.Certificate('./confidential.json')
 default_app = firebase_admin.initialize_app(cred)
 
 db = firestore.client()
