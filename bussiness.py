9ae10209e78b3f12b46f1a694fd88b9349b02e6b
John Holbanel <john.holbanel3@mail.dcu.ie>
2020-03-03 20:07:37 +0000

    quicker fix than before

--- bussiness.py
+++ bussiness.py
@@ -9,6 +9,7 @@ def distance_calculator(lo1, la1, lo2, la2):
     r = requests.get("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + longitude1 + "," + latitude1 + "&destinations=" + longitude2 + "%2C" + latitude2 + "&key=AIzaSyCj2T1Mdd-K7AtQh0SoO9-hWna1h8m-R_E")
     data = r.json()
     time = data["rows"][0]["elements"][0]["duration"]["text"]
+    time = time.split()[0]
     return time
 
 print(distance_calculator("40.6655101", "-73.89188969999998", "40.6905615", "-73.9976592"))
