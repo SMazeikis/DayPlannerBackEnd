713a8c04bb96402a466c42116f2829914c29c8fb
Salvijus Mazeikis <salvijussocial@gmail.com>
2020-03-04 20:40:29 +0000

    added time assigning

--- bussiness.py
+++ bussiness.py
@@ -2,10 +2,11 @@ import requests
 
 
 def distance_calculator(longitude1, latitude1, longitude2, latitude2):
-    r = requests.get("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + longitude1 + "," + latitude1 + "&destinations=" + longitude2 + "%2C" + latitude2 + "&key=AIzaSyCj2T1Mdd-K7AtQh0SoO9-hWna1h8m-R_E")
+    r = requests.get("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + latitude1 + "," + longitude1 + "&destinations=" + latitude2 + "%2C" + longitude2 + "&key=AIzaSyCj2T1Mdd-K7AtQh0SoO9-hWna1h8m-R_E")
     data = r.json()
     time = data["rows"][0]["elements"][0]["duration"]["text"]
     time = time.split()[0]
     return time
 
-print(distance_calculator("40.6655101", "-73.89188969999998", "40.6905615", "-73.9976592"))
+# print(distance_calculator("40.6655101", "-73.89188969999998", "40.6905615", "-73.9976592"))
+# print(distance_calculator("53.3498", "6.2603","53.3373903","-6.239726"))
--- dayPlan.py
+++ dayPlan.py
@@ -10,13 +10,14 @@ import firebase_admin
 import random
 from flask import Flask, request, jsonify, make_response
 from yelp.client import Client
+from bussiness import distance_calculator
 
 MY_API_KEY = "b7wSeZeFykMFbPx-n7VpQD9gwi8EZWYYjaPEoa3ExSZv5c4LlGpANEhKR-sUW51218X6M3RO6t90tQGvbBAcX7pP9A3KnEo8FRfYZ1t1efZ_SXvj7POdAmmyqptTXnYx"
 
 client = Client(MY_API_KEY)
 
 if (not len(firebase_admin._apps)):
-    cred = credentials.Certificate('./confidential.json')
+    cred = credentials.Certificate(r'C:\Users\Salvijus\Desktop\2020-ca326-jholbanel-dayplanner\code\dayplanner-backend\confidential.json')
     default_app = firebase_admin.initialize_app(cred)
 
 db = firestore.client()
@@ -58,6 +59,7 @@ def yelp_business_info(tag, limit=10):
     business_data = response.json()
     for business in business_data['businesses']:
         yelp_business_names[business['name']] = {
+                                'location' : business['coordinates'],
                                 'url' : business['url'],
                                 'tag' :  tag}
     return yelp_business_names
@@ -65,40 +67,42 @@ def yelp_business_info(tag, limit=10):
 
 def arrange_in_order(restaurants, activities, duration):
     day = {}
+    times = [0]
 
-    restaurant_counter = 0
     if duration >= 6:
         restaurant_counter = 2
-        for event_number in range(0, duration):
-            if event_number % 2 == 0 and event_number != 0 and restaurant_counter != 0:
-                restaurant_by_cuisine = random.choice(restaurants)
-                restaurant = restaurant_by_cuisine.popitem()
-                day[event_number] = restaurant
-                restaurant_counter -= 1
-            else:
-                activity_by_option = random.choice(activities)
-                activity = activity_by_option.popitem()
-                day[event_number] = activity
 
     elif duration >= 3:
         restaurant_counter = 1
-        for event_number in range(0, duration):
+
+    else:
+        restaurant_counter = 0
+
+    activity_by_option = random.choice(activities)
+    activity = activity_by_option.popitem()
+    time = distance_calculator("6.2603","53.3498", str(activity[1]['location']['longitude']),  str(activity[1]['location']['latitude']))
+    times.append(time)
+    day[0] = activity
+
+    for event_number in range(1, duration):
+
             if event_number % 2 == 0 and event_number != 0 and restaurant_counter != 0:
                 restaurant_by_cuisine = random.choice(restaurants)
                 restaurant = restaurant_by_cuisine.popitem()
+                last_event_location = str(day[event_number - 1][1]['location']['longitude']), str(day[event_number - 1][1]['location']['latitude'])
+                time = distance_calculator(last_event_location[0], last_event_location[1], str(restaurant[1]['location']['longitude']), str(restaurant[1]['location']['latitude']))
+                times.append(time)
                 day[event_number] = restaurant
                 restaurant_counter -= 1
             else:
                 activity_by_option = random.choice(activities)
                 activity = activity_by_option.popitem()
+                last_event_location = str(day[event_number - 1][1]['location']['longitude']), str(day[event_number - 1][1]['location']['latitude'])
+                time = distance_calculator(last_event_location[0], last_event_location[1], str(activity[1]['location']['longitude']), str(activity[1]['location']['latitude']))
+                times.append(time)
                 day[event_number] = activity
-
-    else:
-        for event_number in range(0, duration):
-            activity_by_option = random.choice(activities)
-            activity = activity_by_option.popitem()
-            day[event_number] = activity
     
+    day["times"] = times
     return day
 
 #print(makeDay(data))
